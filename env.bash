1178ca209cdc8e47da1257d7813cc4a62879f8a0
herocat <1722791503@qq.com>
2023-12-02 17:11:14 +0800

    solve env

--- env.bash
+++ env.bash
@@ -1,17 +1,28 @@
 #! /bin/bash
+
 conda create -n mml python==3.8.5
 conda activate mml
 # conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 pip install -r requirements.txt
+
+# other modules from Github
 makedir src
 cd src
+
 # CLIP
-git clone git@https://github.com/openai/CLIP.git
+git clone git@github.com:openai/CLIP.git
 cd CLIP
 pip install -e .
 cd ..
+
 # mmtrack
 git clone git@github.com:open-mmlab/mmtracking.git
 cd mmtracking
 pip install -e .
-# mmdetection (maybe)
+cd ..
+
+# mmdetection
+git clone git@github.com:open-mmlab/mmdetection.git
+cd mmdetection
+pip install -e .
+cd ..
